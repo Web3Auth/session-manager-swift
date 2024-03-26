49870cb5ef72673d515b492e8742f057f26bb431
ieow <cwlee0@gmail.com>
2024-03-26 15:13:14 +0800

    feat: remove unused code

--- Sources/SessionManager/SessionManager+Extension.swift
+++ Sources/SessionManager/SessionManager+Extension.swift
@@ -26,7 +26,6 @@ extension SessionManager {
 
     func encryptData(privkeyHex: String, _ dataToEncrypt: String) throws -> String {
         let secretKey = try curveSecp256k1.SecretKey(hex: privkeyHex)
-        let pubKey = try secretKey.toPublic().serialize(compressed: false)
         
         let encParams = try encryption_aes_cbc_sha512.Encryption.encrypt(pk: secretKey.toPublic(), plainText: dataToEncrypt)
         
@@ -36,35 +35,4 @@ extension SessionManager {
         return string
     }
 
-    // why do we need this function??
-    private func encParamsBufToHex(encParamsHex: ECIES) throws -> ECIES {
-        let iv = encParamsHex.iv
-        let ephemPublicKey = encParamsHex.ephemPublicKey
-          let ciphertext = encParamsHex.ciphertext
-          let mac = encParamsHex.mac
-        
-        return .init(iv: iv, ephemPublicKey: ephemPublicKey, ciphertext: ciphertext, mac: mac)
-    }
-
-    private func encParamsHexToBuf(encParamsHex: String) throws -> ECIES {
-        let data = encParamsHex.data(using: .utf8) ?? Data()
-        var arr = Array(repeating: "", count: 4)
-        do {
-            let dict = try JSONSerialization.jsonObject(with: data) as? [String: String]
-            dict?.forEach { key, value in
-                if key == "iv" {
-                    arr[0] = value
-                } else if key == "ephemPublicKey" {
-                    arr[1] = value
-                } else if key == "ciphertext" {
-                    arr[2] = value
-                } else if key == "mac" {
-                    arr[3] = value
-                }
-            }
-            return ECIES(iv: arr[0], ephemPublicKey: arr[1], ciphertext: arr[2], mac: arr[3])
-        } catch let error {
-            throw SessionManagerError.runtimeError(error.localizedDescription)
-        }
-    }
 }
