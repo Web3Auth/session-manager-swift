22a10b51c32d13f9acca10e1d55c93798058aea4
metalurgical <97008724+metalurgical@users.noreply.github.com>
2024-10-24 05:48:34 +0200

    update: cocoapods example

--- Example/Pods/Target Support Files/Session-Manager/Session-Manager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Session_Manager : NSObject
-@end
-@implementation PodsDummy_Session_Manager
-@end
---
+++ Example/Pods/Target Support Files/curvelib.swift/curvelib.swift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_curvelib_swift : NSObject
+@end
+@implementation PodsDummy_curvelib_swift
+@end
